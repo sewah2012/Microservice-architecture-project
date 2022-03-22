0d191a8c2f5ced2cdf1081b4abda982c6846c6db
Emmanuel <sewah2012@gmail.com>
2022-03-22 16:10:01 +0100

    push to github

--- src/main/java/com/easy/hystrixdashboard/HystrixDashboardApplication.java
+++ src/main/java/com/easy/hystrixdashboard/HystrixDashboardApplication.java
@@ -3,9 +3,11 @@ package com.easy.hystrixdashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrixDashboard
 public class HystrixDashboardApplication {
 
 	public static void main(String[] args) {
