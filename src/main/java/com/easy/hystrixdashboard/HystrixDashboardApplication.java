d316c20af13ec8717034d1f4f5e417d6a6defbe1
Emmanuel <sewah2012@gmail.com>
2022-03-22 12:24:59 +0100

    setup hystrix dashboard to visualise api

---
+++ src/main/java/com/easy/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,15 @@
+package com.easy.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+
+}
